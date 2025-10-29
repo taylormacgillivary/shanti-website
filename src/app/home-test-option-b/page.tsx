66b2f10a5813f65b7636289fa067c911d2d34b93
Taylor MacGillivary <taylormacgillivary@taylors-mbp.home>
2025-10-29 17:05:16 -0300

    homepage testing

---
+++ src/app/home-test-option-b/page.tsx
@@ -0,0 +1,18 @@
+"use client"
+
+import { HeroSectionVideoScroll } from "@/components/hero-section-video-scroll";
+import { StudiosSection } from "@/components/studios-section";
+import { TeachersSection } from "@/components/teachers-section";
+import { IntroPassSection } from "@/components/intro-pass-section";
+
+export default function HomeTestOptionB() {
+  return (
+    <>
+      <HeroSectionVideoScroll />
+      <StudiosSection />
+      <IntroPassSection />
+      <TeachersSection />
+    </>
+  );
+}
+
---
+++ src/app/home-test/page.tsx
@@ -0,0 +1,18 @@
+"use client"
+
+import { HeroSectionVideo } from "@/components/hero-section-video";
+import { StudiosSection } from "@/components/studios-section";
+import { TeachersSection } from "@/components/teachers-section";
+import { IntroPassSection } from "@/components/intro-pass-section";
+
+export default function HomeTest() {
+  return (
+    <>
+      <HeroSectionVideo />
+      <StudiosSection />
+      <IntroPassSection />
+      <TeachersSection />
+    </>
+  );
+}
+
