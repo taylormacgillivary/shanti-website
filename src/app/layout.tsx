a2335a745246e25a48a462dd90edaae61b8a5581
Taylor MacGillivary <taylormacgillivary@Taylors-MacBook-Pro.local>
2025-11-06 13:26:53 -0400

    analytics componenets added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { RootLayoutClient } from "./client-layout";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -45,6 +46,7 @@ export default function RootLayout({
         suppressHydrationWarning={true}
       >
         <RootLayoutClient>{children}</RootLayoutClient>
+        <Analytics />
       </body>
     </html>
   );
